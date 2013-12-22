95232e5a179419f55fa98f448d6f1d2291c16152
Nick Lowery <nick.a.lowery@gmail.com>
2013-12-22 02:18:26 -0700

    - Adds ability to modify clap interval and clap grouping timeout at runtime. - Adds "songs.txt" functionality: reads in sound files associated with numbers of claps. - Updates readme to explain "songs.txt" functionality.

--- Assets/_Scripts/ClapReaction.cs
+++ Assets/_Scripts/ClapReaction.cs
@@ -18,12 +18,15 @@
 ************************************************************************/
 
 using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class ClapReaction : MonoBehaviour
 {
+    public string SongsFile = "songs.txt";
+
     private Color backgroundColor = Color.black;
     private float colorDecay = 0.98f;
+    private Dictionary<int, AudioFile> clapSongs;
 
     private Clapper clapper;
 
@@ -32,26 +35,79 @@ public class ClapReaction : MonoBehaviour
         clapper = GetComponent<Clapper>();
     }
 
+    private void Start()
+    {
+        ParseSongsFile();
+    }
+
     private void Update()
     {
+        // Fade background color
         backgroundColor *= colorDecay;
         Camera.main.backgroundColor = backgroundColor;
 
+        // Get input to adjust clap threshold
         if (Input.GetKeyDown(KeyCode.A))
         {
-            clapper.ClapVolumeThreshold += 0.01f;
+            clapper.ClapVolumeThreshold += 0.005f;
         }
         else if (Input.GetKeyDown(KeyCode.Z))
         {
-            clapper.ClapVolumeThreshold -= 0.01f;
+            clapper.ClapVolumeThreshold -= 0.005f;
         }
-
         clapper.ClapVolumeThreshold = Mathf.Clamp(clapper.ClapVolumeThreshold, 0.01f, 1);
+
+        // Get input to adjust clap interval
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            clapper.ClapMinInterval += 0.05f;
+        }
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
+            clapper.ClapMinInterval -= 0.005f;
+        }
+        clapper.ClapMinInterval = Mathf.Clamp(clapper.ClapMinInterval, 0, 1);
+
+        // Get input to adjust clap timeout
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            clapper.ClapGroupingTimeout += 0.1f;
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            clapper.ClapGroupingTimeout -= 0.1f;
+        }
+        clapper.ClapMinInterval = Mathf.Clamp(clapper.ClapMinInterval, 0, 1);
     }
 
     private void OnGUI()
     {
-        GUI.Label(new Rect(0, 0, 500, 50), "Clap volume threshold (A/Z keys to modify): " + clapper.ClapVolumeThreshold.ToString());
+        GUI.Label(new Rect(0, 0, 500, 20), "Clap volume threshold (A/Z keys to modify): " + clapper.ClapVolumeThreshold.ToString());
+        GUI.Label(new Rect(0, 20, 500, 20), "Clap min interval (S/X keys to modify): " + clapper.ClapMinInterval.ToString());
+        GUI.Label(new Rect(0, 40, 500, 20), "Clap grouping timeout (D/C keys to modify): " + clapper.ClapGroupingTimeout.ToString());
+    }
+
+    private void ParseSongsFile()
+    {
+        clapSongs = new Dictionary<int, AudioFile>();
+
+        string songsText = System.IO.File.ReadAllText(SongsFile).Trim();
+
+        string[] lines = songsText.Split(new string[] { ";" }, System.StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (string line in lines)
+        {
+            string[] tokens = line.Trim().Split('@');
+
+            // Get number of claps
+            int numClaps = int.Parse(tokens[0].Trim());
+
+            // Get song file
+            string songFile = tokens[1].Trim();
+
+            print(numClaps + ", " + songFile);
+            clapSongs.Add(numClaps, new AudioFile(songFile));
+        }
     }
 
     public void Clap(float volume)
@@ -59,15 +115,20 @@ public class ClapReaction : MonoBehaviour
         backgroundColor += 2 * volume * Color.white;
     }
 
-    public void Claps2()
+    public void ClapGroup(int numClaps)
     {
-        if (!audio.isPlaying)
+        if (clapSongs.ContainsKey(numClaps))
         {
-            audio.Play();
-        }
-        else
-        {
-            audio.Stop();
+            if (audio.isPlaying &&
+                audio.clip == clapSongs[numClaps].Clip)
+            {
+                audio.Stop();
+            }
+            else
+            {
+                audio.clip = clapSongs[numClaps].Clip;
+                audio.Play();
+            }
         }
     }
 }
