a6226e954cdd9f35b6294d00fda864a0e8b139be
Nick Lowery <ClockVapor@users.noreply.github.com>
2013-12-24 03:13:05 -0700

    - Adds slider bars for GUI, and also centers GUI on screen. - Adds PlayerPrefs support to save clapper settings.

--- Assets/_Scripts/ClapReaction.cs
+++ Assets/_Scripts/ClapReaction.cs
@@ -24,9 +24,10 @@ public class ClapReaction : MonoBehaviour
 {
     public string SongsFile = "songs.txt";
 
-    private Color backgroundColor = Color.black;
+    private Color backgroundColor = Color.gray;
     private float colorDecay = 0.98f;
     private Dictionary<int, AudioFile> clapSongs;
+    public GUIStyle TextStyle;
 
     private Clapper clapper;
 
@@ -37,14 +38,32 @@ public class ClapReaction : MonoBehaviour
 
     private void Start()
     {
+        // Load player prefs
+        if (PlayerPrefs.HasKey("volume"))
+        {
+            clapper.ClapVolumeThreshold = PlayerPrefs.GetFloat("volume");
+        }
+        if (PlayerPrefs.HasKey("interval"))
+        {
+            clapper.ClapMinInterval = PlayerPrefs.GetFloat("interval");
+        }
+        if (PlayerPrefs.HasKey("timeout"))
+        {
+            clapper.ClapGroupingTimeout = PlayerPrefs.GetFloat("timeout");
+        }
+
+        Camera.main.backgroundColor = backgroundColor;
         ParseSongsFile();
     }
 
     private void Update()
     {
         // Fade background color
-        backgroundColor *= colorDecay;
-        Camera.main.backgroundColor = backgroundColor;
+        Camera.main.backgroundColor *= colorDecay;
+        Camera.main.backgroundColor = new Color(
+            Mathf.Clamp(Camera.main.backgroundColor.r, backgroundColor.r, 1),
+            Mathf.Clamp(Camera.main.backgroundColor.g, backgroundColor.g, 1),
+            Mathf.Clamp(Camera.main.backgroundColor.b, backgroundColor.b, 1));
 
         // Get input to adjust clap threshold
         if (Input.GetKeyDown(KeyCode.A))
@@ -57,17 +76,6 @@ public class ClapReaction : MonoBehaviour
         }
         clapper.ClapVolumeThreshold = Mathf.Clamp(clapper.ClapVolumeThreshold, 0.01f, 1);
 
-        // Get input to adjust clap interval
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            clapper.ClapMinInterval += 0.05f;
-        }
-        else if (Input.GetKeyDown(KeyCode.X))
-        {
-            clapper.ClapMinInterval -= 0.005f;
-        }
-        clapper.ClapMinInterval = Mathf.Clamp(clapper.ClapMinInterval, 0, 1);
-
         // Get input to adjust clap timeout
         if (Input.GetKeyDown(KeyCode.D))
         {
@@ -77,14 +85,44 @@ public class ClapReaction : MonoBehaviour
         {
             clapper.ClapGroupingTimeout -= 0.1f;
         }
-        clapper.ClapMinInterval = Mathf.Clamp(clapper.ClapMinInterval, 0, 1);
+        clapper.ClapGroupingTimeout = Mathf.Clamp(clapper.ClapGroupingTimeout, 0.1f, 1);
+        clapper.ClapMinInterval = Mathf.Clamp(clapper.ClapMinInterval, 0, clapper.ClapGroupingTimeout - 0.1f);
+
+        // Get input to adjust clap interval
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            clapper.ClapMinInterval += 0.05f;
+        }
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
+            clapper.ClapMinInterval -= 0.05f;
+        }
+        clapper.ClapMinInterval = Mathf.Clamp(clapper.ClapMinInterval, 0, 0.9f);
+        clapper.ClapGroupingTimeout = Mathf.Clamp(clapper.ClapGroupingTimeout, clapper.ClapMinInterval + 0.1f, 1);
     }
 
     private void OnGUI()
     {
-        GUI.Label(new Rect(0, 0, 500, 20), "Clap volume threshold (A/Z keys to modify): " + clapper.ClapVolumeThreshold.ToString());
-        GUI.Label(new Rect(0, 20, 500, 20), "Clap min interval (S/X keys to modify): " + clapper.ClapMinInterval.ToString());
-        GUI.Label(new Rect(0, 40, 500, 20), "Clap grouping timeout (D/C keys to modify): " + clapper.ClapGroupingTimeout.ToString());
+        GUI.Label(new Rect(0, -80, Screen.width, Screen.height), "Clap amplitude threshold (A/Z keys to modify): " + clapper.ClapVolumeThreshold.ToString(), TextStyle);
+        GUI.Label(new Rect(0, -20, Screen.width, Screen.height), "Clap min interval (S/X keys to modify): " + clapper.ClapMinInterval.ToString() + " seconds", TextStyle);
+        GUI.Label(new Rect(0, 40, Screen.width, Screen.height), "Clap grouping timeout (D/C keys to modify): " + clapper.ClapGroupingTimeout.ToString() + " seconds", TextStyle);
+
+        clapper.ClapVolumeThreshold = GUI.HorizontalSlider(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 60, 200, 20), clapper.ClapVolumeThreshold, 0.01f, 1.0f);
+        clapper.ClapMinInterval = GUI.HorizontalSlider(new Rect(Screen.width / 2 - 100, Screen.height / 2, 200, 20), clapper.ClapMinInterval, 0, 1);
+        clapper.ClapMinInterval = Mathf.Clamp(clapper.ClapMinInterval, 0, 0.9f);
+        clapper.ClapGroupingTimeout = Mathf.Clamp(clapper.ClapGroupingTimeout, clapper.ClapMinInterval + 0.1f, 1);
+        clapper.ClapGroupingTimeout = GUI.HorizontalSlider(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 60, 200, 20), clapper.ClapGroupingTimeout, 0, 1);
+        clapper.ClapGroupingTimeout = Mathf.Clamp(clapper.ClapGroupingTimeout, 0.1f, 1);
+        clapper.ClapMinInterval = Mathf.Clamp(clapper.ClapMinInterval, 0, clapper.ClapGroupingTimeout - 0.1f);
+    }
+
+    private void OnApplicationQuit()
+    {
+        // Save player prefs
+        PlayerPrefs.SetFloat("volume", clapper.ClapVolumeThreshold);
+        PlayerPrefs.SetFloat("interval", clapper.ClapMinInterval);
+        PlayerPrefs.SetFloat("timeout", clapper.ClapGroupingTimeout);
+        PlayerPrefs.Save();
     }
 
     private void ParseSongsFile()
@@ -105,14 +143,17 @@ public class ClapReaction : MonoBehaviour
             // Get song file
             string songFile = tokens[1].Trim();
 
-            print(numClaps + ", " + songFile);
+#if UNITY_EDITOR
+            print(numClaps + " claps: " + songFile);
+#endif
+
             clapSongs.Add(numClaps, new AudioFile(songFile));
         }
     }
 
     public void Clap(float volume)
     {
-        backgroundColor += 2 * volume * Color.white;
+        Camera.main.backgroundColor += 2 * volume * Color.white;
     }
 
     public void ClapGroup(int numClaps)
